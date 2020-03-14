ba9fe97d4ed3581c79363bd93e031b24e4b7015d
nachourpi <nachourpi@hotmail.com>
2020-03-14 16:46:03 -0300

    Init react app and added firebase package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { LineChart, Line } from 'recharts';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+const data = [
+  {name: 'Page A', uv: 400, pv: 145, amt: 2400}, 
+  {name: 'Page A', uv: 500, pv: 333, amt: 2400}, 
+  {name: 'Page A', uv: 600, pv: 125, amt: 2400}, 
+  {name: 'Page A', uv: 400, pv: 984, amt: 2400}, 
+  {name: 'Page A', uv: 240, pv: 415, amt: 2400}, 
+]
+
+
+export default class App extends React.Component {
+  componentDidMount() {
+    //if (Platform.OS == 'ios') {
+      console.log("Component mounted")
+    //}
+  }
+
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,9 +37,13 @@ function App() {
         >
           Learn React
         </a>
+        <LineChart width={400} height={400} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <Line type="monotone" dataKey="pv" stroke="#a7d3d8" />
+        </LineChart>
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
